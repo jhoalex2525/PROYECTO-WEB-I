b1b6d7f9341cac352b781f7b1ca6efa2bf12a9fb
jhonatan.ossa <proyectosfzv>
2021-04-17 09:39:05 -0500

    V1.1 Se crea branch branch-jhonatan y se modifican las columnas para el responsive

--- pages/apartmentowner.php
+++ pages/apartmentowner.php
@@ -1,20 +1,20 @@
 <?php include_once("../static/layouts/header.php") ?>
 <h1>Apartamentos por propietario</h1>
 <div class="row">
-    <div class="col-6 picturetype1align">
+    <div class="col-md-6 picturetype1align">
         <img class="picturetype1" src="../images/room1.jpeg" alt="">
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <form>
-            <div class="mb-6">
+            <div class="mb-3">
                 <label class="form-label">Número de habitaciones:</label>
                 <input type="text" class="form-control" id="numberadd" name="numberadd" selected disabled>
             </div>
-            <div class="mb-6">
+            <div class="mb-3">
                 <label class="form-label">Valor noche:</label>
                 <input type="text" class="form-control" id="valueadd" name="valueadd" selected disabled>
             </div>
-            <div class="mb-6">
+            <div class="mb-3">
                 <label class="form-label">Reseña habitación:</label>
                 <textarea type="text" class="form-control" style="height:125px" id="roomreviewsignup" name="roomreviewsignup" selected disabled></textarea>
             </div>
--- pages/profile.php
+++ pages/profile.php
@@ -1,20 +1,20 @@
 <?php include_once("../static/layouts/header.php") ?>
 <h1>Perfil Anfitrión</h1>
 <div class="row">
-    <div class="col-6 picturetype1align">
+    <div class="co-mdl-6 picturetype1align">
         <img class="picturetype1" src="../images/defaultprofile.png" alt="">
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <form>
-            <div class="mb-6">
+            <div class="mb-3">
                 <label class="form-label">Nombre completo:</label>
                 <input type="text" class="form-control" id="name" name="name" selected disabled>
             </div>
-            <div class="mb-6">
+            <div class="mb-3">
                 <label class="form-label">Ciudad:</label>
                 <input type="text" class="form-control" id="town" name="town" selected disabled>
             </div>
-            <div class="mb-6">
+            <div class="mb-3">
                 <label class="form-label">Reseña personal:</label>
                 <textarea type="text" class="form-control" style="height:125px" id="reviewsignup" name="reviewsignup" selected disabled></textarea>
             </div>
