b935e124e23ac7ec2974003efb3a6ac237e99f85
CHARLESDEVELOPER <89650239+CHARLESDEVELOPER@users.noreply.github.com>
2021-10-19 19:17:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     $apartments = $apartmentModel -> getApartmentsForIndex();    
     while($row = mysqli_fetch_assoc($apartments)){    
     $template = "
+    //ejemplo charles//
     <div class='row index'>
         <div class='col-md-6 mb-3 picturetype1align'>
             <div>    
